8b591b3fd7fba5f6ed74829b7b56ac6e8481222c
dbeley <6568955+dbeley@users.noreply.github.com>
2019-05-12 02:09:02 +0200

    add config sample

--- get_ids.py
+++ get_ids.py
@@ -38,7 +38,7 @@ def main():
     if not type:
         logger.error("-t/--type argument required. Exiting.")
         exit()
-    elif type not in ["all", "owned", "wishlist"]:
+    elif type not in ["all", "owned", "wishlist", "both"]:
         logger.error(f"Type {type} not supported. Exiting.")
         exit()
 
@@ -76,7 +76,7 @@ def main():
             f.write("appid\tName")
             for game in json_dict['applist']['apps']:
                 f.write(f"{game['appid']}\t{game['name']}\n")
-    if type == 'owned':
+    elif type == 'owned':
         logger.debug("Type : owned")
         url = f"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={api_key}&steamid={user_id}&format=json"
         json_dict = requests.get(url).json()
@@ -91,9 +91,9 @@ def main():
                 dict_game['playtime_2weeks'] = '0'
             dict_games.append(dict_game)
 
-            df = pd.DataFrame(dict_games)
-            df.to_csv(f"Exports/ids_owned_games_{user_id}.csv", sep='\t')
-    if type == 'wishlist':
+        df = pd.DataFrame(dict_games)
+        df.to_csv(f"Exports/ids_owned_games_{user_id}.csv", sep='\t')
+    elif type == 'wishlist':
         logger.debug("Type : wishlist")
         url = f"https://store.steampowered.com/wishlist/profiles/{user_id}/wishlistdata/?p=0"
         json_dict = requests.get(url).json()
@@ -129,6 +129,26 @@ def main():
 
         df = pd.DataFrame(dict_games)
         df.to_csv(f"Exports/wishlist_{user_id}.csv", sep='\t')
+    elif type == 'both':
+        logger.debug("Type : both")
+        url = f"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={api_key}&steamid={user_id}&format=json"
+        json_dict = requests.get(url).json()
+        dict_games = []
+        for game in json_dict['response']['games']:
+            dict_game = {}
+            dict_game['appid'] = game['appid']
+            dict_games.append(dict_game)
+
+        url = f"https://store.steampowered.com/wishlist/profiles/{user_id}/wishlistdata/?p=0"
+        json_dict = requests.get(url).json()
+
+        for game_id in json_dict:
+            dict_game = {}
+            dict_game['appid'] = game_id
+            dict_games.append(dict_game)
+
+        df = pd.DataFrame(dict_games)
+        df.to_csv(f"Exports/both_{user_id}.csv", sep='\t')
 
     logger.info("Runtime : %.2f seconds" % (time.time() - temps_debut))
 
@@ -136,7 +156,7 @@ def main():
 def parse_args():
     parser = argparse.ArgumentParser(description='Steam script')
     parser.add_argument('--debug', help="Display debugging information", action="store_const", dest="loglevel", const=logging.DEBUG, default=logging.INFO)
-    parser.add_argument('-t', '--type', help="Type of ids to export (all, owned or wishlist)", type=str)
+    parser.add_argument('-t', '--type', help="Type of ids to export (all, owned, wishlist or both (owned and wishlist))", type=str)
     parser.add_argument('-u', '--user_id', help="User id to extract the games info from (steamID64)", type=str)
     parser.set_defaults(boolean_flag=False)
     args = parser.parse_args()
